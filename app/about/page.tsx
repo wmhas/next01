98b69fc0e94367af5dc1706bc594f0669dc58d45
whashim <whashim@gmail.com>
2023-07-08 14:05:57 +0800

    Add existing project files prior to the push to GitHub.

---
+++ app/about/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (
+       <div><h1>about</h1></div>
+       
+    )
+  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+//import './globals.css'
 
 export default function RootLayout({
   children,
@@ -12,7 +12,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav><h1>my nav bar</h1></nav>
+        {children}</body>
     </html>
   )
 }
